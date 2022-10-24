f221f9182fc5c7936b630b99f3c0e184cc3d16ea
Jay <jmartinsmith@outlook.com>
2022-10-24 14:38:48 +0100

    update deploys

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import 'solidity-coverage';
+import '@nomiclabs/hardhat-ethers';
+import 'dotenv/config';
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_URL,
+      accounts: [process.env.ACCOUNT_KEY || '']
+    }
+  }
 };
 
 export default config;
