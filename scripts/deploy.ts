2d0f9a57463a2199dc42fa8677776cee87c2c708
Jay <jmartinsmith@outlook.com>
2022-10-24 13:15:10 +0100

    clean up hardhat scripts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,14 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = ethers.utils.parseEther("1");
 
   const MemorialWallContract = await ethers.getContractFactory("MemorialWallet");
-  const lock = await MemorialWallContract.deploy(unlockTime, { value: lockedAmount });
+  const memwall = await MemorialWallContract.deploy(unlockTime, { value: lockedAmount });
 
-  await lock.deployed();
+  await memwall.deployed();
 
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  console.log(`Mem wall deployed to ${memwall.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
